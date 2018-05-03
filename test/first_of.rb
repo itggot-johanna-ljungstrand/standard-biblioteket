96a4dfcec946be48c78de27b958355201a989120
itggot-johanna-ljungstrand <38528666+itggot-johanna-ljungstrand@users.noreply.github.com>
2018-05-03 14:52:50 +0200

    added first_of.rb

---
+++ test/first_of.rb
@@ -0,0 +1,14 @@
+# Public: Returns the element in the array.
+#
+# array  - One Array to check.
+#
+# Examples
+#
+#   first_of([1,2,3])
+#   # => 1
+#
+# Returns the element in the array.
+def first_of(array)
+    output = array[0]
+    return output
+end
